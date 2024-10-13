b3557c84d377a4c59bd7a6bd5c5d2679a69ed152
DPthang411 <dinhphuocthang46@gmail.com>
2024-10-13 13:21:46 +0700

    add files

---
+++ build/CMakeFiles/OPENGL.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OPENGL.dir/main.cpp.obj"
+  "CMakeFiles/OPENGL.dir/main.cpp.obj.d"
+  "OPENGL.exe"
+  "OPENGL.exe.manifest"
+  "OPENGL.pdb"
+  "libOPENGL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OPENGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
